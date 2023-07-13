ed6f4761bb927d13141028cf32fbaac209f7f476
Noé Breton <noe.breton@etu.univ-tours.fr>
2023-07-13 12:24:36 +0300

    add a simulation controller script

---
+++ Assets/Scripts/Controller/SimulationController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimulationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private List<Slider> sliders;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
